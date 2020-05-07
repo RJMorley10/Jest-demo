162fd0b1949f24a1d7b310f4f3ea5627a7a96ec3
Digidev <rj.morley@elevationscu.com>
2020-05-07 15:00:14 -0600

    ready for demo

--- src/demo.js
+++ src/demo.js
@@ -1,8 +1,31 @@
 function isPalindrome(word){
-
+    var splitWord = word.split("")
+    var reverseWord = splitWord.reverse()
+    var newWord = reverseWord.join("")
+    if(newWord === word){
+        return true
+    }
+    else{
+        return false
+    }
 }
 
-function Add(num1, num2){
-
+// This function checks an array of strings to see if the code word for toilet paper shows up
+// Note: there are a couple things wrong with this function use the tests you create to figure out what is wrong 
+// with the function and fix it.
+function checkForMessage(messageArray){
+    console.log(messageArray.length)
+    var secretMessage = "No toilet paper"
+    for (var i = 1; i < messageArray.length - 1; i++){
+        console.log(messageArray[i])
+        if(messageArray[i] === "twilight books"){
+            console.log("found")
+            secretMessage = "we have toilet paper"
+        }
+    }
+    console.log(secretMessage)
+    return secretMessage
 }
 
+exports.isPalindrome = isPalindrome
+exports.checkForMessage = checkForMessage
---
+++ src/demo.test.js
@@ -0,0 +1,51 @@
+const demo = require('./demo')
+
+test("Check if the word is a palindrome", () => {
+    expect(demo.isPalindrome("racecar").toBeTruthy)
+
+})
+
+test("Check if the word is a not palindrome", () => {
+    expect(demo.isPalindrome("yellow").toBeFalsy)
+    
+})
+
+test("Check if a sentance is a palindrome", () => {
+    // write test to check if a sentance is a palindrome
+
+})
+
+test("see if the number 212 is a palindrome", () => {
+    // write test to check id 212 is a palindrome
+})
+
+
+describe("check to see if the store has toilet paper", () => {
+    test("decode the secret list to see if the grocery store has toilet paper", () => {
+        var secretList = [
+            'coffee',
+            'rice',
+            'paper towels',
+            'we have toilet paper',
+            'milk',
+            'cheese',
+        ]
+        const verificationString = "No toilet paper"
+        expect(demo.checkForMessage(secretList)).toEqual(verificationString)
+    })
+})
+
+describe("Lets see if they have toilet paper today", () => {
+    test("decode the secret list to see if the grocery store has toilet paper", () => {
+        var secretList = [
+            'coffee',
+            'rice',
+            'paper towels',
+            'we have toilet paper',
+            'milk',
+            'cheese',
+            'twilight books'
+        ]
+        // finish this test to see if the store now has toilet paper 
+    })
+})
